a64bc4afb958bff2ba680b22b02b30f1710c1ab4
ok4f <karachev_og@mail.ru>
2022-07-11 08:50:59 +0200

    fix

--- ln.sh
+++ ln.sh
@@ -9,17 +9,17 @@ rm -v ${dirCron}/*/*
 
 for cron in ${min5Cron[@]}; do
   ln -s $dirScript/$cron $dirCron/5min/
-  echo "enable ${crone} to 5min"
+  echo "enable ${cron} to 5min"
 done
 
 for cron in ${daysCron[@]}; do
   ln -s $dirScript/$cron $dirCron/day/
-  echo "enable ${crone} to day"
+  echo "enable ${cron} to day"
 done
 
 for cron in ${weekCron[@]}; do
   ln -s $dirScript/$cron $dirCron/week/
-  echo "enable ${crone} to week"
+  echo "enable ${cron} to week"
 done
 
 printf "\n"
