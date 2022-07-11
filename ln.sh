8b6101e4e8c0a666113977f1a014044530ca34f4
ok4f <karachev_og@mail.ru>
2022-07-11 08:55:10 +0200

    ln

--- ln.sh
+++ ln.sh
@@ -9,17 +9,17 @@ rm -v ${dirCron}/*/*
 
 for cron in ${min5Cron[@]}; do
   ln -s $dirScript/$cron $dirCron/5min/
-  echo "enable ${cron} to 5min"
+  echo "Включено ${cron} Каждые 5 минут"
 done
 
 for cron in ${daysCron[@]}; do
   ln -s $dirScript/$cron $dirCron/day/
-  echo "enable ${cron} to day"
+  echo "Включено ${cron} раз в день"
 done
 
 for cron in ${weekCron[@]}; do
   ln -s $dirScript/$cron $dirCron/week/
-  echo "enable ${cron} to week"
+  echo "Включено ${cron} раз в неделю"
 done
 
 printf "\n"
